ebbd76a2b74f09d0ec89ab86876f86152db611b3
Glenn Ou <2738328919@qq.com>
2020-09-27 19:10:45 +0800

    Code fix: add comma in the "v1Router.use('api/v1/auth', authRouter)"

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
 import Express from 'express'
+
 import Mongoose from 'mongoose'
+
 import config from '@config'
+
+import path from 'path'
+
 import v1Router from '@routes'
+
+import WebpackConfig from '../webpack.config'
+
+import WebpackDevMiddleware from 'webpack-dev-middleware'
+
+
 
 Mongoose.connect(config.databaseUrl, { useNewUrlParser: true })
 
 const app = Express()
 
+const compiler = webpack(webpackConfig)
+
+app.use(WebpackDevMiddleware(compiler))
+
 app.use(v1Router)
+
+app.get('*', () => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('server started succesfully.')
